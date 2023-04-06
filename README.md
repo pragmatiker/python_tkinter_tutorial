de4f34e90cd7690ba2e0705e2d2ff05f24d5832b
Tim Herty <tim.herty@gmail.com>
2023-04-06 12:05:49 +0200

    betta baattten

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # python_tkinter_tutorial
+https://www.youtube.com/playlist?list=PLCC34OHNcOtoC6GglhF3ncJ5rLwQrLGnV
--- entry.py
+++ entry.py
@@ -3,11 +3,17 @@ from tkinter import *
 
 root = Tk()
 
-# create entry
-e = Entry(root,width=50)
+e = Entry(root,width=50,fg="green",borderwidth=5)
 e.pack()
+e.insert(0,"Enter your Name")
+e.focus()
 
+def myClick():
+    hello = "Hello " + e.get()
+    myLabel = Label(root, text=hello)
+    myLabel.pack()
+
+myButton = Button(root, text="Print greeting", padx=5,pady=5,command=myClick,fg="black",bg="grey",activebackground="white")
+myButton.pack()
 
 root.mainloop()
-
-
