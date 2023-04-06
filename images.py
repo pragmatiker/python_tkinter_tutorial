98ffbf811243a5e832c56b8d088b95441ea724f5
Tim Herty <tim.herty@gmail.com>
2023-04-06 20:41:07 +0200

    we have learned enough for today

--- images.py
+++ images.py
@@ -9,5 +9,12 @@ root.title('Learn to use images')
 p1 = PhotoImage(file=os.path.join(sys.path[0],'vlc-48.png'))
 root.iconphoto(False,p1)
 
+
+myImage =  ImageTk.PhotoImage(Image.open('/home/therty/Downloads/IMG-20220825-WA0015.jpg'))
+myLabel = Label(image=myImage)
+myLabel.pack()
+
+
 root.mainloop()
+
 
