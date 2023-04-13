af770bf782192bd2307cf7d71454d621d93cf682
Tim Herty <tim.herty@gmail.com>
2023-04-13 20:20:03 +0200

    dd

--- button.py
+++ button.py
@@ -10,5 +10,5 @@ def myClick():
 myButton = Button(root, text="Click Me!", padx=5,pady=5,command=myClick,fg="black",bg="grey",activebackground="white")
 myButton.pack()
 
-root.mainloop()
+root.mainloop() 
 
