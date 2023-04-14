3897d52238fdd420c91cdfb1d363e560cf09651b
Tim Herty <tim.herty@gmail.com>
2023-04-14 11:43:46 +0200

    me learn tkinter configure

--- tkinter/checkbox.py
+++ tkinter/checkbox.py
@@ -8,7 +8,6 @@ root = Tk()
 root.title('Learn to use images')
 p1 = PhotoImage(file=os.path.join(sys.path[0],'vlc-48.png'))
 root.iconphoto(False,p1)
-#root.geometry("600x400")
 
 root.columnconfigure(0, weight=2)
 root.columnconfigure(1, weight=1)
@@ -16,15 +15,12 @@ root.columnconfigure(2, weight=2)
 var = StringVar()
 
 def show():
-    #global bSelectBackdrop
     if var.get() == "on":
-        bSelectBackdrop = Button(root, text="Select File",state=ACTIVE)
-        myLabel = Text(root, height=1, state=NORMAL, relief=SUNKEN, bd=4)
+        bSelectBackdrop.configure(state=NORMAL)
+        myLabel.configure(state=NORMAL)
     if var.get() == "off":
-        bSelectBackdrop = Button(root, text="Select File",state=DISABLED)
-        myLabel = Text(root, height=1, state=DISABLED, background="grey", relief=SUNKEN, bd=2)
-    myLabel.grid(row=0,column=1,sticky=NSEW)
-    bSelectBackdrop.grid(row=0,column=2,sticky=E)
+        bSelectBackdrop.configure(state=DISABLED)
+        myLabel.configure(state=DISABLED)
 
 c = Checkbutton(root, text="Use Backdrop", variable=var, onvalue="on",offvalue="off",command=show)
 c.deselect()
