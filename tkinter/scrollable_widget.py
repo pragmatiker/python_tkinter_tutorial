2635fa6fb21ce64bd2f6b8fcfa69df43c6d2a507
Tim Herty <tim.herty@gmail.com>
2023-04-16 10:59:29 +0200

    modified:   tkinter/scrollable_widget.py

--- tkinter/scrollable_widget.py
+++ tkinter/scrollable_widget.py
@@ -13,27 +13,40 @@ class ListFrame(ttk.Frame):
         self.list_height = self.item_number * item_height
 
         # canvas
-        self.canvas = tk.Canvas(self, background='red', scrollregion=(0,0,500,self.list_height))
+        self.canvas = tk.Canvas(self, background='red', scrollregion=(0,0,self.winfo_width(),self.list_height))
         self.canvas.pack(expand=True, fill='both')
 
         # frame inside canvas that holds the widgets
         self.frame = ttk.Frame(self)
 
+        # create the widgets from list 
         for index, item in enumerate(self.text_data):
             self.create_item(index, item).pack(expand=True, fill='both',pady=4, padx=10)
+ 
+        # events ...
+        self.canvas.bind_all('<Button-4>', lambda event: self.canvas.yview_scroll(-1, "units"))
+        self.canvas.bind_all('<Button-5>', lambda event: self.canvas.yview_scroll(1, "units"))
+        self.bind('<Configure>', self.update_size)
 
+    # resize canvas
+    def update_size(self,event):
+        if self.list_height >= self.winfo_height():
+            height = self.list_height
+            self.canvas.bind_all('<Button-4>', lambda event: self.canvas.yview_scroll(-1, "units"))
+            self.canvas.bind_all('<Button-5>', lambda event: self.canvas.yview_scroll(1, "units"))
+        else:
+            height = self.winfo_height()
+            self.canvas.unbind_all('<Button-4>')
+            self.canvas.unbind_all('<Button-5>')
+                                   
         self.canvas.create_window(
             (0,0),
             window = self.frame,
             anchor='nw',
-            width=500,
-            height=self.list_height
-            )
+            width=self.winfo_width(),
+            height=height)
         
-        # events ...
-        self.canvas.bind_all('<Button-4>', lambda event: self.canvas.yview_scroll(-1, "units"))
-        self.canvas.bind_all('<Button-5>', lambda event: self.canvas.yview_scroll(1, "units"))
-
+    # method for ceating the widgets
     def create_item(self, index, item):
         frame = ttk.Frame(self.frame)
 
@@ -62,8 +75,11 @@ text_list = [('label','button'),
              ('label7','button7'),
              ('label8','button8'),
              ('label9','button9'),
+             ('label10','button10'),
+             ('label11','button11'),
+             ('label12','button12'),
              ]
-list_frame = ListFrame(window, text_list, 100)
+list_frame = ListFrame(window, text_list, 60)
 
 
 # run
