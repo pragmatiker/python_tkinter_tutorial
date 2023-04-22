c40979856f8c24df16c7c0ac2cfc3bee445af52a
Tim Herty <tim.herty@gmail.com>
2023-04-22 09:57:31 +0200

    entry fun

--- tkinter/entry.py
+++ tkinter/entry.py
@@ -1,19 +1,25 @@
 #!/usr/bin/env python3
-from tkinter import *
+import tkinter as tk
+from tkinter import ttk
 
-root = Tk()
+root = tk.Tk()
+style = ttk.Style()
+style.configure('new.TButton')
+style.map('new.TButton',
+          foreground = [('pressed','green'),('disabled', 'yellow')],
+          background = [('pressed','red'),('disabled', 'blue')])
 
-e = Entry(root,width=50,fg="green",borderwidth=5)
+e = ttk.Entry(root,width=50,foreground='green')
 e.pack()
 e.insert(0,"Enter your Name")
 e.focus()
 
 def myClick():
     hello = "Hello " + e.get()
-    myLabel = Label(root, text=hello)
+    myLabel = ttk.Label(root, text=hello, background='green', justify='left')
     myLabel.pack()
 
-myButton = Button(root, text="Print greeting", padx=5,pady=5,command=myClick,fg="black",bg="grey",activebackground="white")
+myButton = ttk.Button(root, text="Print greeting",padding=(5,5),command=myClick, style='new.TButton')
 myButton.pack()
 
 root.mainloop()
